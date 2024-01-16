3ccefa2487c2fcf54d87eeb4cd12d11e8b5767ad
Isaiah Apreku <isaiahapreku@isaiahs-air-2.mynetworksettings.com>
2024-01-16 04:07:22 -0500

    sqls

--- config/config.js
+++ config/config.js
@@ -12,3 +12,16 @@ const sequelize = process.env.JAWSDB_URL
     });
 
 module.exports = sequelize;
+
+// config.json
+{
+  "development"; {
+    "username"; process.env.DB_USER,
+    "password"; process.env.DB_PASSWORD,
+    "database"; process.env.DB_NAME,
+    "host"; "localhost",
+    "dialect"; "mysql",
+    "port"; 3306
+  }
+  // ... (similar configurations for other environments like "test" and "production")
+}
